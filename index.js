98688c2772dc67e0b65a27e72ae97a87420c8892
Adam <experimental@example.com>
2025-04-17 14:54:59 +0500

    started to creat a framework of backend

---
+++ index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
+import { validationResult } from "express-validator";
+
+import { registerValidation } from "./validations/auth.js";
+
+mongoose
+  .connect(
+    "mongodb+srv://admin:wwwwww@cluster0.ayfpcci.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => console.log("DB ok"))
+  .catch((err) => console.log("Db error".err));
+
+const app = express();
+
+app.use(express.json());
+
+app.post("/auth/register", registerValidation, (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json(errors.array());
+  }
+
+  res.json({
+    success: true,
+  });
+});
+
+app.listen(4444, (err) => {
+  if (err) {
+    return console.log(err);
+  }
+
+  console.log("Server OK");
+});
