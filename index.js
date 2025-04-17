5c0e68988cb0fa4acac3a0cbce6f08352a5dffbf
Adam <experimental@example.com>
2025-04-17 20:23:42 +0500

    wrote user creation logic and fixed some typos

--- index.js
+++ index.js
@@ -1,30 +1,44 @@
 import express from "express";
-import jwt from "jsonwebtoken";
 import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 import { validationResult } from "express-validator";
 
 import { registerValidation } from "./validations/auth.js";
+
+import UserModel from "./models/User.js";
 
 mongoose
   .connect(
     "mongodb+srv://admin:wwwwww@cluster0.ayfpcci.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
   )
   .then(() => console.log("DB ok"))
-  .catch((err) => console.log("Db error".err));
+  .catch((err) => console.log("Db error", err));
 
 const app = express();
 
 app.use(express.json());
 
-app.post("/auth/register", registerValidation, (req, res) => {
+app.post("/auth/register", registerValidation, async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json(errors.array());
   }
 
-  res.json({
-    success: true,
+  const password = req.body.password;
+  const salt = await bcrypt.genSalt(10);
+  const passwordHash = await bcrypt.hash(password, salt);
+
+  const doc = new UserModel({
+    email: req.body.email,
+    fullName: req.body.fullName,
+    avatarUrl: req.body.avatarUrl,
+    passwordHash,
   });
+
+  const user = await doc.save();
+
+  res.json(user);
 });
 
 app.listen(4444, (err) => {
